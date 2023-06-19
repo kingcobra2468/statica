4d6f7953cb68d22a4a9978b12bfa72012594f881
erik <eriktyryshkin@gmail.com>
2023-06-18 23:52:29 -0700

    Add cython setuptools pipeline

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from setuptools import setup, Extension
+import os
+
+from Cython.Build import cythonize
+
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    print(extensions)
+    return extensions
+
+
+extensions = [
+    Extension("statica.file.encoder", ["src/statica/file/encoder.pyx"]),
+    Extension("statica.file.decoder", ["src/statica/file/decoder.pyx"]),
+]
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
+
+with open("requirements.txt") as fp:
+    install_requires = fp.read().strip().split("\n")
+
+
+setup(
+    ext_modules=extensions,
+    install_requires=install_requires,
+)
--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-from Cython.Build import cythonize
-
-setup(
-    name='statica',
-    packages=find_packages(),
-    ext_modules=cythonize(
-        ['./**/file/*.py', './**/file/*.pxd'], language_level='3')
-)
